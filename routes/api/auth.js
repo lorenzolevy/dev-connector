db3640a275af5342cbc18e9917107ee281c00857
Lorenzo Levy <lorenzoroccolevy@gmail.com>
2020-07-26 19:23:55 -0700

    Can now make get requests to api with x-auth-token for user info

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,22 @@
 const express = require("express");
+// Middleware to be passed into router.get()
+const auth = require("../../middleware/auth");
 // Init a router
 const router = express.Router();
 
+const User = require("../../models/User");
+
 // @route  GET api/auth
-// @desc   Test route
+// @desc   Test
 // @access Public -- no token needed
-router.get("/", (req, res) => res.send("Auth route"));
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(300).send("Server error");
+  }
+});
 
 module.exports = router;
