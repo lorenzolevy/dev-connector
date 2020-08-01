d38d8b35395253b0722f473d597e252fd86654ac
Lorenzo Levy <lorenzoroccolevy@gmail.com>
2020-08-01 12:40:03 -0700

    adeed posts listview and ability to make POST requests with new posts, and delete requests for only your own posts

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,8 @@ import Profile from "./components/profile/Profile";
 import AddExperience from "./components/profile-forms/AddExperience";
 import AddEducation from "./components/profile-forms/AddEducation";
 
+import Posts from "./components/posts/Posts";
+
 import PrivateRoute from "./components/routing/PrivateRoute";
 import "./App.css";
 
@@ -64,6 +66,7 @@ const App = () => {
                 path='/add-education'
                 component={AddEducation}
               />
+              <PrivateRoute exact path='/posts' component={Posts} />
             </Switch>
           </section>
         </Fragment>
